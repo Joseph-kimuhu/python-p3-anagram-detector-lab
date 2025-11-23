c84101cb0ecff97b6e999d99229d45cee425e4d4
Joseph-Kimuhu <joseph.kimuhu@student.moringaschool.com>
2025-11-24 00:33:52 +0300

    first commit

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,6 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+    
+    def match(self, possible_anagrams):
+        return [word for word in possible_anagrams if sorted(word.lower()) == sorted(self.word.lower())]
